2cf349f0ca15d02f493aaaeeb9c28676f972d004
Digital Alchemist <developer.anwar@gmail.com>
2017-07-25 22:28:48 -0400

    Code Correction
    Array size is initialized 1+ userdefined value. Fixed Now.

--- algorithms-app/algorithms-dp/src/main/java/com/dc/algorithms/dp/memorization/Factorial.java
+++ algorithms-app/algorithms-dp/src/main/java/com/dc/algorithms/dp/memorization/Factorial.java
@@ -1,5 +1,7 @@
 package com.dc.algorithms.dp.memorization;
 
+import java.util.Arrays;
+
 public class Factorial {
 
 	public static long getFactorialUsingMemorization(long userInput){
@@ -7,19 +9,20 @@ public class Factorial {
 		if(userInput < 0)
 			throw new IllegalArgumentException();
 		
-		long[] memory = new long[(int) userInput+1];
-		
+		long[] memory = new long[(int) userInput];
 		long factorial = 1;
 		
 		if(userInput == 0 || userInput == 1){
-			memory[0] = 1;
 			return factorial;
 		}
+		
+		memory[0] = 1;
 		
 		for (int i = 2; i <= userInput; i++) {
 			memory[i-1] = factorial = factorial * i;
 		}
 		
+		System.out.println(Arrays.toString(memory));
 		return memory[(int) userInput-1];
 	}
 }
