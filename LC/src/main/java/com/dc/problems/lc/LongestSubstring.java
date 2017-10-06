aa398cd34a1bafecf3bcbf0efe737373a6a5a737
Digital Alchemist <developer.anwar@gmail.com>
2017-10-05 21:49:11 -0400

    Longest Substring
    1- Length of Non Repeating Chars

---
+++ LC/src/main/java/com/dc/problems/lc/LongestSubstring.java
@@ -0,0 +1,19 @@
+package com.dc.problems.lc;
+
+import java.util.Arrays;
+
+public class LongestSubstring {
+	public int getLengthLongestSubstringWithoutRepeatingCharacters(String userText) {
+		int length = 0;
+
+		int[] charMap = new int[256];
+		Arrays.fill(charMap, -1);
+		
+		for (int i = 0; i < charMap.length; i++) {
+			
+		}
+		
+		
+		return length;
+	}
+}
